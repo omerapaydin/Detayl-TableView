ec004d6fc16871581be9d96cf46494f67b5c4ed2
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-07 17:34:54 +0300

    Initial Commit

---
+++ DetaylıTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetaylıTableView
+//
+//  Created by Ömer on 7.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DetaylıTableViewTests/Detayl_TableViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  Detayl_TableViewTests.swift
+//  DetaylıTableViewTests
+//
+//  Created by Ömer on 7.04.2025.
+//
+
+import Testing
+@testable import Detayl_TableView
+
+struct Detayl_TableViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

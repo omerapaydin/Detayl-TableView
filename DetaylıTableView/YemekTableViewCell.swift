b753ec96498957da9e5a1a43bb7c9454615b9149
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-07 23:37:21 +0300

    Yemek sınıfı

--- DetaylıTableView/YemekTableViewCell.swift
+++ DetaylıTableView/YemekTableViewCell.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class YemekTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var yemekname: UILabel!
+    @IBOutlet weak var ücretText: UILabel!
+    @IBOutlet weak var imageYemek: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,4 +24,6 @@ class YemekTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    @IBAction func siparişVer(_ sender: Any) {
+    }
 }

ba28cea5891a842964deabbb138ee267c4320d50
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-07 23:37:00 +0300

    Yemek sınıfı

---
+++ DetaylıTableView/YemekTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  YemekTableViewCell.swift
+//  DetaylıTableView
+//
+//  Created by Ömer on 7.04.2025.
+//
+
+import UIKit
+
+class YemekTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
